31f2b197d9595ae2c7a25baae77c1390616953f0
joao vieira <john.collins.vieira@gmail.com>
2023-07-05 19:26:47 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const writeCards = (array,event) => {
+    const messages = []
+    for(let i = 0; i < array.length;i++) {
+        
+           const greetingMessage = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+    }
+    return messages
+
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  };
+  
+
